8afdf2665210b7f0c7b055b568e688a3a331e7e6
Jocowl25 <jocowl25@gmail.comgit config --global user.email jocowl25@gmail.comgit config --global user.email jocowl25@gmail.com>
2026-05-16 11:56:41 -0400

    fetch answer from input file

---
+++ index.js
@@ -0,0 +1,47 @@
+initialize();
+async function initialize(){
+    let answerJSON=await getAnswer();
+    console.log(answerJSON);
+}
+
+async function getAnswer(){
+    try{
+    const response=await fetch("input.txt");
+    const data=await response.text();
+    return JSON.parse(data);
+    }catch{
+        alert("Failed to fetch input data!")
+    }
+   
+}
+
+function getAbility(input){
+    switch(input){
+        case 0:return"Prevents Death";
+        case 1:return"On Death";
+        case 2:return"Execution";
+        case 3:return"Causes Death";
+        case 4:return"Droisoning";
+        case 5:return"Learns Character";
+        case 6:return"Selects Character";
+        case 7:return"Specific Character";
+        case 8:return"Yes/No";
+        case 9:return"Learns Number";
+        case 10:return"Public";
+        case 11:return"Nomination/Voting";
+        case 12:return"Once/First Time";
+        case 13:return"Setup";
+        case 14:return"Madness";
+        case 15:return"Alignment";
+        case 16:return"Outsiders";
+        case 17:return"Minions";
+        case 18:return"Townsfolk";
+        case 19:return"Demon";
+        case 20:return"Win/Loss";
+        case 21:return"Changes Character";
+        case 22:return"Can Revive";
+        case 23:return"Positioning";
+        case 24:return"Storyteller";
+        case 25:return"?"
+    }
+}
